2814e2a9953bba5ab5fcbf55fcd293963f6e7b0d
Guilherme David <137658009+Guilherme-David@users.noreply.github.com>
2026-02-09 15:51:11 -0300

    Update Courses.jsx

--- frontend/src/pages/Courses.jsx
+++ frontend/src/pages/Courses.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import api from "../services/api";
+import { api } from "../services/api";
 import Navbar from "../components/Navbar";
 import "../index.css";
 import { Link } from "react-router-dom";
