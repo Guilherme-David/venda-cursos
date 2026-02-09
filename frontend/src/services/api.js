5e4bfffcffb6fc8143b4eb0190e8e77fa6df6ddd
Guilherme David <137658009+Guilherme-David@users.noreply.github.com>
2026-02-09 15:48:41 -0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://localhost:5000",
-});
+export const API_URL = process.env.REACT_APP_API_URL;
 
-export default api;
+export const api = axios.create({
+  baseURL: API_URL,
+});
