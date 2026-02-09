da0e5a6b3c5bd61a2f8e0a8d0412fa70ae387896
Guilherme David <137658009+Guilherme-David@users.noreply.github.com>
2026-02-09 15:53:24 -0300

    Update HomeList.jsx

--- frontend/src/components/HomeList.jsx
+++ frontend/src/components/HomeList.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import api from "../services/api";
+import { api } from "../services/api";
 import "../index.css";
 
 export default function HomeList() {
